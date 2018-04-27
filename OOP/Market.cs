7699be104923925d4375fd0b7068875c06e9b547
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-04-27 03:02:49 +0300

    Create Market.cs

---
+++ OOP/Market.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices
+{
+    class Product
+    {
+        public readonly string Name;
+        public readonly string Barcode;
+        private double value;
+        private double quantity;
+
+        public double Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public Product(string name, string barcode)
+        {
+            this.Name = name;
+            this.Barcode = barcode;
+        }
+
+    }
+    static class Market
+    {
+        public static List<Product> products = new List<Product>();   
+        
+        public static Product Add(string barcode, string name, double value, double quantity)
+        {
+            products.Add(new Product(name, barcode) { Quantity = quantity, Value = value });
+            return products.Last();
+        }
+
+        public static Product Update(string barcode, double newQuantity)
+        {
+            if(!products.Exists(x => x.Barcode == barcode))
+            {
+                Console.WriteLine("Please add your product first!");
+                return null;
+            }
+            int currProductIndex = products.IndexOf(products.Find(x => x.Barcode == barcode));
+            products[currProductIndex].Quantity = newQuantity;
+            return products[currProductIndex];
+        }
+
+        public static void Sell(string barcode, double quantity)
+        {
+            if (products.Exists(x => x.Barcode == barcode))
+            {
+
+                Product toSell = products.Find(x => x.Barcode == barcode);
+                if (toSell.Quantity >= quantity)
+                {
+                    Market.Update(toSell.Barcode, toSell.Quantity - quantity);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough quantity");
+                }
+            }
+            else
+            {
+                throw new Exception("No such a product");
+            }
+        }
+
+        public static List<Product> Accessible()
+        {
+            return products.Where(x => x.Quantity > 0).ToList();
+        }
+
+        public static List<Product> Unaccessible()
+        {
+            return products.Except(Market.Accessible()).ToList();
+        }
+
+        public static List<Product> SortByDecending()
+        {
+            return products.OrderByDescending(x => x.Quantity).ToList();
+        }
+
+        public static double Calculate()
+        {
+             return products.Select(x => x.Quantity).
+                Zip(products.Select(y => y.Value), (a, b) => a * b).Sum();
+        }
+    }
+}
